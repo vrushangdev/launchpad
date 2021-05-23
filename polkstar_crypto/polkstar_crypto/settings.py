1c21b39f77d05dd704ae612ed05685d4260281b4
Vrushang <vrushangdev@gmail.com>
2021-05-23 17:32:08 +0530

    * Added Static settings

--- polkstar_crypto/polkstar_crypto/settings.py
+++ polkstar_crypto/polkstar_crypto/settings.py
@@ -131,11 +131,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR,'polkstar_crypto/static/')
+# ]
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'polkstar_crypto/static/')
-]
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
 
 #Media Folder Settings
 
